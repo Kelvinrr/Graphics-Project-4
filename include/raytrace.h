ebc6b4e23b6de38fd1f9c4a44bf5eaae29a68e4f
Kelvin Rodriguez <kelvinrr@icloud.com>
2016-12-12 00:12:11 -0700

    comments4dayz

--- include/raytrace.h
+++ include/raytrace.h
@@ -12,17 +12,32 @@
 #include "pplib.h"
 #include "illumination.h"
 
-#define SHININESS 30
+#define SHININESS 999999
 #define MAX_REC_LEVEL 5
 
+// Struct to represent both position and direction
 typedef struct Ray {
-        double origin[3];
-        double direction[3];
+        V3 origin;
+        V3 direction;
 } Ray;
 
+// Default background color
 V3 background_color = {0, 0, 0};
 
-int get_camera(OBJECT *objects) {
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+  Searches an array of objects and returns the index of the camera.
+  Returns -1 if not camera is found.
+
+  Parameters
+  ==========
+  OBJECT objects
+         An OBJECT array containing different objects
+  Returns
+  =======
+  size_t index
+         The index of the camera object
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+size_t get_camera(OBJECT *objects) {
         int i = 0;
         while (i < MAX_OBJECTS && objects[i].type != 0) {
                 if (objects[i].type == CAM) {
@@ -34,14 +49,50 @@ int get_camera(OBJECT *objects) {
         return -1;
 }
 
-void shade_pixel(double *color, int row, int col,Image *image){
-        image->data[row * image->width*4 + col*4] = (u_char)(MAX_COLOR_VAL * check_value(color[0]));
-        image->data[row * image->width*4 + col*4+1] = (u_char)(MAX_COLOR_VAL* check_value(color[1]));
-        image->data[row * image->width*4 + col*4+2]= (u_char)(MAX_COLOR_VAL* check_value(color[2]));
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+  Sets the color value for a pixel in an image
+
+  Parameters
+  ==========
+  double color
+         The color used to set
+  size_t row
+         The pixel row
+  size_t col
+         The pixel column
+  Image image
+        The image the pixel comes from
+
+  Returns
+  =======
+  void
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+static inline void shade_pixel(double *color, size_t row, size_t col, Image *image) {
+        image->data[row * image->width*4 + col*4] = (uint8_t)(MAX_COLOR_VAL * check_value(color[0]));
+        image->data[row * image->width*4 + col*4+1] = (uint8_t)(MAX_COLOR_VAL* check_value(color[1]));
+        image->data[row * image->width*4 + col*4+2]= (uint8_t)(MAX_COLOR_VAL* check_value(color[2]));
         image->data[row * image->width*4 + col*4+3]= 255;
 }
 
-double plane_intersection(Ray *ray, double *Pos, double *Norm){
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+  Computes if ray intersects with plane, returns the distance of
+  intersection if so.
+
+  Parameters
+  ==========
+  Ray ray
+      The ray used to compute the intersection with an object
+  double pos
+         The position of the plane
+  V3 norm
+     Plane surface normal
+
+  Returns
+  =======
+  double distance
+         The distance of intersection
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+double plane_intersection(Ray *ray, double *Pos, V3 Norm){
         double alph,delta;
         normalize(Norm);
 
@@ -66,7 +117,25 @@ double plane_intersection(Ray *ray, double *Pos, double *Norm){
 
 }
 
-double sphere_intersection(Ray *ray, double *C, double r) {
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+  Computes if ray intersects with sphere, returns the distance of
+  intersection if so.
+
+  Parameters
+  ==========
+  Ray ray
+      The ray used to compute the intersection with an object
+  V3 center
+     The center of the sphere
+  double r
+         The radius of sphere
+
+  Returns
+  =======
+  double distance
+         The distance of intersection
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+double sphere_intersection(Ray *ray, V3 C, double r) {
         double a, b;
         V3 vector_diff;
         v3_subtract(ray->origin, C, vector_diff);
@@ -90,28 +159,53 @@ double sphere_intersection(Ray *ray, double *C, double r) {
         return t;
 }
 
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+  Gets the location of intersection.
 
-void get_intersection(double* intersection, Ray *ray, double t){
+  Parameters
+  ==========
+  V3 intersection
+     new vector with intersection position vector
+  Ray ray
+      Position used to compute intersection
+  double t
+         distance of intersection
+
+  Returns
+  =======
+  void
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+void get_intersection(V3 intersection, Ray *ray, double t){
         intersection[0] = ray->origin[0] + ray->direction[0] * t;
         intersection[1] = ray->origin[1] + ray->direction[1] * t;
         intersection[2] = ray->origin[2] + ray->direction[2] * t;
 }
 
-void get_quadric_normal(V3 normal ,double *Coefficient, double *Position) {
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+  Computes the object that intersects closest to some ray
 
-        normal[0] =-2*Coefficient[0]*(Position[0])-Coefficient[3]*(Position[1])-Coefficient[4]*(Position[2])-Coefficient[6];
-        normal[1] =-2*Coefficient[1]*(Position[1])-Coefficient[3]*(Position[0])-Coefficient[5]*(Position[2])-Coefficient[7];
-        normal[2] =-2*Coefficient[2] * (Position[2])-Coefficient[4]*(Position[0])- Coefficient[5] * (Position[1])-Coefficient[8];
-        normalize(normal);
+  Parameters
+  ==========
+  Ray ray
+      The ray to use for computing the intersection with an object
+  size_t self_index
+         index of object to find the solution for
+  double max_distance
+         Farthest intersection distance acceptable
+  size_t* ret_index
+         The variable to store the index of the best solution object
+  double* ret_best_t
+          The variable to store the distance of intersection with the best solution object
 
-}
-
+  Returns
+  =======
+  void
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 void get_best_solution(Ray *ray, int self_index, double max_distance, int *ret_index, double *ret_best_t) {
         int best_o = -1;
         double best_t = INFINITY;
         for (int i=0; objects[i].type != 0; i++) {
-                // if self_index was passed in as > 0, we must ignore object i because we are checking distance to another
-                // object from the one at self_index.
+                // ignore self
                 if (self_index == i) continue;
 
                 // we need to run intersection test on each object
@@ -145,6 +239,22 @@ void get_best_solution(Ray *ray, int self_index, double max_distance, int *ret_i
         (*ret_best_t) = best_t;
 }
 
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+  Get the surface normal of object at intersection location
+
+  Parameters
+  ==========
+  size_t object_index
+         The index of the object in the object array
+  V3 position
+     The position to compute the normal for
+  V3 normal
+     The variable to store the final normal
+
+  Returns
+  =======
+  void
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 void get_normal(int object_index, V3 position, V3 normal){
         if (objects[object_index].type == PLAN) {
                 v3_copy(objects[object_index].plane.normal, normal);
@@ -155,7 +265,20 @@ void get_normal(int object_index, V3 position, V3 normal){
         }
 }
 
-double get_reflectivity(int object_index){
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+  Accessor for reflectivity
+
+  Parameters
+  ==========
+  size_t object_index
+         The index of the object in the object array
+
+  Returns
+  =======
+  double reflectivity
+         The reflectivity of the object
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+double get_reflectivity(size_t object_index){
         if (objects[object_index].type == PLAN) {
                 return objects[object_index].plane.reflect;
         }
@@ -168,6 +291,19 @@ double get_reflectivity(int object_index){
         }
 }
 
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+  Accessor for refractivity
+
+  Parameters
+  ==========
+  size_t object_index
+         The index of the object in the object array
+
+  Returns
+  =======
+  double refractivity
+         The refractivity of the object
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 double get_refractivity(int object_index) {
         if (objects[object_index].type == PLAN) {
                 return objects[object_index].plane.refract;
@@ -181,6 +317,19 @@ double get_refractivity(int object_index) {
         }
 }
 
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ Accessor for the index of refraction
+
+ Parameters
+ ==========
+ size_t object_index
+        The index of the object in the object array
+
+ Returns
+ =======
+ double ior
+        The index of refraction of the object
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 double get_ior(int object_index){
         double ior;
         if (objects[object_index].type == PLAN) {
@@ -190,7 +339,7 @@ double get_ior(int object_index){
                 ior = objects[object_index].sphere.ior;
         }
         else {
-                fprintf(stderr, "Error: get_ior: Can't find ior property for this project\n");
+                fprintf(stderr, "Error: get_ior: Can't find ior property for this object\n");
                 exit(1);
         }
         if (fabs(ior) < 0.0001)
@@ -199,6 +348,24 @@ double get_ior(int object_index){
                 return ior;
 }
 
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ Get the reflected vector of object at intersection location
+ from some direction
+
+ Parameters
+ ==========
+ size_t object_index
+ The index of the object in the object array
+ V3 position
+ The position to compute the normal for
+ V3 normal
+ The variable to store the final reflected vector
+
+ Returns
+ =======
+ void
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 void reflection_vector(V3 direction, V3 position, int object_index, V3 reflection) {
         V3 normal;
         get_normal(object_index, position, normal);
@@ -206,6 +373,29 @@ void reflection_vector(V3 direction, V3 position, int object_index, V3 reflectio
         v3_reflect(direction, normal, reflection);
 }
 
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ Get the refraction vector of object at intersection location
+ from some direction
+
+ Parameters
+ ==========
+ V3 direction
+    Direction of vector to refract
+ V3 position
+    position of vector to refract
+ int object_index
+     The index of the object which the ray is intersection, its
+     properties define the final vector
+ double out_ior
+        The output index of refraction
+ V3 refracted_vector
+    The final vector, it is stored in this variable
+
+ Returns
+ =======
+ void
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 void refraction_vector(V3 direction, V3 position, int object_index, double out_ior, V3 refracted_vector) {
         // initializations and variables setup
         V3 dir, pos;
@@ -217,9 +407,10 @@ void refraction_vector(V3 direction, V3 position, int object_index, double out_i
         double in_ior = get_ior(object_index);
 
         if (in_ior == out_ior) {
-                in_ior =1;
+                in_ior = 1;
         }
 
+        // define all the normals
         V3 normal, a, b;
 
         // find normal vector of current object
@@ -242,6 +433,29 @@ void refraction_vector(V3 direction, V3 position, int object_index, double out_i
         v3_add(normal, b, refracted_vector);
 }
 
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ Simple raycast function, computes intersection between a ray an an object,
+ then computes the final color based on the object's properties
+
+ Parameters
+ ==========
+ Ray* ray
+      The ray used in ray casting
+ int object_index
+     The index of the object to compute the color for
+ V3 position
+     the position of intersection with the object
+ LIGHT light
+       The light used used to compute lighting
+ double max_dist
+        The max light distance
+ V3 color
+    Where the final color is stored
+
+ Returns
+ =======
+ void
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 void cast(Ray *ray, int object_index, V3 position, LIGHT *light, double max_dist, V3 color) {
         V3 normal;
         V3 object_diff_color;
@@ -298,7 +512,30 @@ void cast(Ray *ray, int object_index, V3 position, LIGHT *light, double max_dist
         color[2] += frad * fang * (specular_color[2] + diffuse_color[2]);
 }
 
-void trace(Ray *ray, int object_index, double t,double current_ior,int rec_level, V3 color) {
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ Simple raycast function, computes intersection between a ray an an object,
+ then computes the final color based on the object's properties
+
+ Parameters
+ ==========
+ Ray* ray
+      The ray used in ray casting
+ int object_index
+     The index of the object to compute the color for
+ double t
+     intersection distance to object
+ double current_ior
+        The current index of refraction
+ int rec_level
+     How many times for the ray to bounce off
+ V3 color
+    Where the final color is stored
+
+ Returns
+ =======
+ void
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+void trace(Ray *ray, int object_index, double t, double current_ior, int rec_level, V3 color) {
         if (rec_level > MAX_REC_LEVEL) {
                 // if the recursive times more than MAX Times, then we return black color
                 v3_scale(color, 0, color);
@@ -336,7 +573,6 @@ void trace(Ray *ray, int object_index, double t,double current_ior,int rec_level
         int best_refraction_object_index;
         double best_refraction_t;
 
-
         Ray ray_reflected = {
                 .origin = {new_origin[0], new_origin[1], new_origin[2]},
                 .direction = {reflection[0], reflection[1], reflection[2]}
@@ -369,7 +605,7 @@ void trace(Ray *ray, int object_index, double t,double current_ior,int rec_level
 
         }
         else {
-                // we have an intersection, so we use recursively shade...
+                // we have an intersection, so we use recursive
                 V3 reflection_color ={0,0,0};
                 V3 refraction_color ={0,0,0};
                 double reflect_coefficient = get_reflectivity(object_index);
@@ -460,6 +696,24 @@ void trace(Ray *ray, int object_index, double t,double current_ior,int rec_level
         }
 }
 
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ Draws a complete scene using a recursive raytracing techinique
+
+ Parameters
+ ==========
+ Image* img
+        The image to draw the scene on
+ double cam_width
+        The width of the camera
+ double cam_height
+        The height of the camera
+ OBJECT* objects
+         The array of objects to draw
+
+ Returns
+ =======
+ void
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 void raytrace_scene(Image *img, double cam_width, double cam_height, OBJECT *objects) {
 
         V3 vp_pos = {0, 0, 1}; // view plane position
--- include/vector_math.h
+++ include/vector_math.h
@@ -6,28 +6,26 @@
 #include <math.h>
 #include "json_parser.h"
 
-typedef double V3[3]; // array of double values to represent
+// Vector type
+typedef double V3[3];
 
-
-
-double sqr(double v){
+static inline double sqr(double v){
     return v*v;
 }
 
-
-void v3_zero(V3 vector){
+static inline void v3_zero(V3 vector){
     vector[0] = 0;
     vector[1] = 0;
     vector[2] = 0;
 }
 
-void v3_copy(V3 in, V3 out){
+static inline void v3_copy(V3 in, V3 out){
     out[0] = in[0];
     out[1] = in[1];
     out[2] = in[2];
 }
 
-void normalize(double *v) {
+static inline void normalize(double *v) {
     double len = sqr(v[0]) + sqr(v[1]) + sqr(v[2]);
     len = sqrt(len);
     v[0] /= len;
@@ -36,38 +34,38 @@ void normalize(double *v) {
 }
 
 
-double v3_len(V3 a){
+static inline double v3_len(V3 a){
     return sqrt(sqr(a[0])+sqr(a[1])+sqr(a[2]));
 }
 
 
-void v3_add(V3 a, V3 b, V3 c){
+static inline void v3_add(V3 a, V3 b, V3 c){
     c[0] = a[0] + b[0];
     c[1] = a[1] + b[1];
     c[2] = a[2] + b[2];
 }
 
 
-void v3_subtract(V3 a, V3 b, V3 c){
+static inline void v3_subtract(V3 a, V3 b, V3 c){
     c[0] = a[0] - b[0];
     c[1] = a[1] - b[1];
     c[2] = a[2] - b[2];
 }
 
 
-void v3_scale(V3 a, double s, V3 b){
+static inline void v3_scale(V3 a, double s, V3 b){
     b[0] = s * a[0];
     b[1] = s * a[1];
     b[2] = s * a[2];
 }
 
 
-double v3_dot(V3 a, V3 b){
+static inline double v3_dot(V3 a, V3 b){
     return a[0]*b[0] + a[1]*b[1] + a[2]*b[2];
 }
 
 
-void v3_cross(V3 a, V3 b, V3 c){
+static inline void v3_cross(V3 a, V3 b, V3 c){
     c[0] = a[1]*b[2] - a[2]*b[1];
     c[1] = a[2]*b[0] - a[0]*b[2];
     c[2] = a[0]*b[1] - a[1]*b[0];
